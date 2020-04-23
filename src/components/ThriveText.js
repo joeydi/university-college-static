cdd42d5812acdfb80e4a4decacaf816f347cc41a
Joe di Stefano <joeydi@okaypl.us>
2020-04-22 22:54:04 -0400

    add ThriveText component

--- src/components/ThriveText.js
+++ src/components/ThriveText.js
@@ -1,11 +1,26 @@
 import React from "react";
 
-const ThriveText = () => {
+const ThriveText = ({ style, content }) => {
   return (
-    <section className="section-padding debug">
-      <div className="container">ThriveText</div>
+    <section className="thrive-text section-padding">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-md-10 col-lg-8">
+            <div style={style}>{content}</div>
+          </div>
+        </div>
+      </div>
     </section>
   );
+};
+
+ThriveText.defaultProps = {
+  style: {
+    textAlign: "center",
+    fontWeight: "600",
+  },
+  content:
+    "University College connects the dots for diverse learner journeys, leveraging technology to personalize learning experiences and support undergraduate student experiences to help them thrive.",
 };
 
 export default ThriveText;
