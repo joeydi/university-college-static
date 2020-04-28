180a6cce9fc52469dbf2b385ccadedea7983252d
Joe di Stefano <joeydi@okaypl.us>
2020-04-28 01:51:44 -0400

    add masonry layout

--- src/components/ThriveMasonryGrid.js
+++ src/components/ThriveMasonryGrid.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import Masonry from "masonry-layout";
+import imagesLoaded from "imagesloaded";
 
-const ThriveMasonryGrid = ({ cards }) => {
-  return (
-    <section className="thrive-masonry-grid section-margin">
-      <div className="container">
-        <div className="row">
-          {cards.map((card, index) => (
-            <div key={index} className="col-sm-6 col-lg-4 mb-20">{card.component(card)}</div>
-          ))}
+class ThriveMasonryGrid extends Component {
+  componentDidUpdate() {
+    const masonry = new Masonry(this.masonryEl, {
+      itemSelector: ".grid-item",
+      percentPosition: true,
+    });
+
+    imagesLoaded(this.masonryEl).on("progress", () => {
+      masonry.layout();
+    });
+  }
+
+  render() {
+    return (
+      <section className="thrive-masonry-grid section-margin">
+        <div className="container">
+          <div className="row grid" ref={(el) => (this.masonryEl = el)}>
+            {this.props.cards.map((card, index) => (
+              <div key={index} className="grid-item col-sm-6 col-lg-4 mb-20">
+                {card.component(card)}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 export default ThriveMasonryGrid;
